7fc03f9e8d94528795d9611c69b2fd369775de8f
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2023-06-14 14:46:09 +0530

    op

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
   <a href="#!" class="brand-logo right"><h5 style={{ color: "Red", display: "flex", fontFamily: "monospace" }}>TIN
                             <h5 style={{ color: "white" }}>DEV</h5>
                         </h5></a>
-  <ul class="left hide-on-med-and-down">
+  <ul class="left">
     <li><a href="sass"><i class="material-icons">search</i></a></li>
     <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
     <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
